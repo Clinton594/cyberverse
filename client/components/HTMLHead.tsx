8b1749bdb76c055b68263741fc3bb5b574b0df43
Onuigbo Clinton <32768102+Clinton594@users.noreply.github.com>
2022-05-05 10:35:12 +0100

    starting with chakra

--- client/components/HTMLHead.tsx
+++ client/components/HTMLHead.tsx
@@ -5,16 +5,16 @@ import projectConfig from '../constants/project.config'
 export default function HtmlHead() {
   return (
     <Head>
-        <title>{projectConfig.name}</title>
-        <meta name="description" content={projectConfig.description} />
-        <meta name="keywords" content={projectConfig.keywords} />
-        <link rel="icon" href={projectConfig.favicon} />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;500;700&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
+      <title>{projectConfig.name}</title>
+      <meta name="description" content={projectConfig.description} />
+      <meta name="keywords" content={projectConfig.keywords} />
+      <link rel="icon" href={projectConfig.favicon} />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;500;700&display=swap"
+        rel="stylesheet"
+      ></link>
+    </Head>
   )
 }
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react"
 import HtmlHead from '../components/HtmlHead'
 
 function MyApp({ Component, pageProps }) {
   return (
-      <>
+      <ChakraProvider>
         <HtmlHead/>
         <Component {...pageProps} />
-      </>
+      </ChakraProvider>
   )
 }
 
